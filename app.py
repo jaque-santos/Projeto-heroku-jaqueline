16e969a526ac54d78d554280413174e3f2d5027e
Jaqueline-santoos <87211727+jaque-santos@users.noreply.github.com>
2021-07-13 10:06:00 -0300

    Atualização final

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 def helloWorld():
     return 'Hello World!'
 
-@app.route('/home')
-def home():
+@app.route('/index')
+def index():
     data = datetime.datetime.now()
-    usuarios = ['Flávio', 'Isabella', 'João', 'Maria']
+    usuarios = ['Jaqueline', 'rute', 'Ewerton', 'Emyle', 'luize']
+    Lista=['https://www.youtube.com/embed/3fP541Qhfd0','https://www.youtube.com/embed/CTIs_RSPr84']
+
     mostrarUsuarios = True
-    return render_template('home.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios)
+    return render_template('index.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios, Lista = Lista)
 
 #app.run(host='0.0.0.0', port=5000)
